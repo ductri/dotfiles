d22a1d0132bc7b34fe3afa57f4bf224427e89d30
Tri Nguyen <ductricse@gmail.com>
2020-04-21 12:38:39 -0700

    soft link does not work, so just gather all dotfile here

---
+++ sync.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo "Save vimrc"
+cp ~/.vimrc vimrc
+echo 
+
+echo "Save tmux.conf"
+cp ~/.tmux.conf tmux.conf
+echo 
+
+echo "Save UltiSnips"
+mkdir -p UltiSnips
+cp ~/.vim/UltiSnips/* UltiSnips/
+echo 
+
+echo "Save latexmkrc"
+cp ~/.latexmkrc latexmkrc
+echo 
+
