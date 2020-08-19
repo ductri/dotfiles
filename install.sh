5ef1e3b7e9636f635ee0fae6b23e4c0e4e13d96d
Tri Nguyen <ductricse@gmail.com>
2020-08-19 11:22:04 -0700

    ok

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+echo "Install vimrc"
+cp vimrc ~/.vimrc
+echo 
+
+echo "Install tmux.conf"
+cp tmux.conf ~/.tmux.conf
+echo 
+
+echo "Install UltiSnips"
+cp UltiSnips/* ~/.vim/UltiSnips/
+echo 
+
+echo "Install latexmkrc"
+cp latexmkrc ~/.latexmkrc 
+echo 
+
+echo "Install zathurarc"
+cp zathurarc ~/.config/zathura/zathurarc 
+echo 
+
+echo "Not install bashrc"
+echo 
+
+echo "Not install  bash_aliases"
+echo 
+
